c6efdcf70cb654045679becf05a8426b99c3341d
wxl <17638228838@163.com>
2025-06-04 00:05:13 +0800

    升级版本号

--- build.gradle.kts
+++ build.gradle.kts
@@ -47,7 +47,8 @@ dependencies {
     // IntelliJ Platform Gradle Plugin Dependencies Extension - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-dependencies-extension.html
     intellijPlatform {
 //        create(providers.gradleProperty("platformType"), providers.gradleProperty("platformVersion"))
-        local("D:\\Program Files\\JetBrains\\IntelliJ IDEA 2025.1.1.1")
+//        local("D:\\Program Files\\IntelliJ IDEA 2025.1.1.1")
+        local(providers.gradleProperty("ideaHome"))
 
         // Plugin Dependencies. Uses `platformBundledPlugins` property from the gradle.properties file for bundled IntelliJ Platform plugins.
         bundledPlugins(providers.gradleProperty("platformBundledPlugins").map { it.split(',') })
